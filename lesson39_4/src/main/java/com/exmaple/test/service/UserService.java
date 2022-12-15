878bb6cffc353973ab0374492bec36de32b49866
Andrey Kozel <andrey.kozel@spanning.com>
2022-12-15 03:03:16 +0300

    Added state to the service

--- lesson39_4/src/main/java/com/exmaple/test/service/UserService.java
+++ lesson39_4/src/main/java/com/exmaple/test/service/UserService.java
@@ -1,5 +1,9 @@
 package com.exmaple.test.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
 import com.exmaple.test.dto.UpdateUserDto;
 import com.exmaple.test.dto.UserDto;
 import lombok.extern.slf4j.Slf4j;
@@ -9,25 +13,42 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
 
+  private final AtomicLong idSequence = new AtomicLong();
+  private final List<UserDto> users = new ArrayList<>();
+
   public UserDto get(final Long userId) {
-    log.info("Get user. Id=[{}]", userId);
-    return UserDto.builder()
-      .id(userId)
-      .firstName("First")
-      .lastName("Last")
-      .password("Pass")
-      .build();
+    return users.stream()
+      .filter(u -> u.getId().equals(userId))
+      .findFirst()
+      .orElse(null);
   }
 
   public void save(final UpdateUserDto dto) {
-    log.info("Create user. Dto=[{}]", dto);
+    final UserDto user = UserDto.builder()
+      .id(idSequence.incrementAndGet())
+      .firstName(dto.getFirstName())
+      .lastName(dto.getLastName())
+      .password(dto.getPassword())
+      .build();
+
+    users.add(user);
   }
 
   public void update(final Long userId, final UpdateUserDto dto) {
-    log.info("Update user. Id=[{}] Dto=[{}]", userId, dto);
+    UserDto userDto = get(userId);
+    users.remove(userDto);
+    final UserDto user = UserDto.builder()
+      .id(userDto.getId())
+      .firstName(dto.getFirstName())
+      .lastName(dto.getLastName())
+      .password(dto.getPassword())
+      .build();
+
+    users.add(user);
   }
 
   public void delete(final Long userId) {
-    log.info("Delete user. Id=[{}]", userId);
+    UserDto userDto = get(userId);
+    users.remove(userDto);
   }
 }
